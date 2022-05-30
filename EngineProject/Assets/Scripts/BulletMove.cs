9a5ec69c5dd0a3e6f8c766c69d242e8ff9125918
dksgudwn <dksgudwn620@gmail.com>
2022-05-30 23:53:22 +0900

    2022년5월30일

---
+++ EngineProject/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float speed = 5f;
+    private void Update()
+    {
+        Vector3 dir = new Vector3(0,1,0);
+        transform.position += dir * speed * Time.deltaTime;
+
+    }
+}
--- EngineProject/Assets/Scripts/PlayerController.cs
+++ EngineProject/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Movement2D movement2D;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject bulletPrefab;
     private void Awake()
     {
-        movement2D = GetComponent<Movement2D>();
+
+    }
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 position = new Vector3(x,y,0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, position, Quaternion.identity);
+        }
     }
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(x,y,0);
-        transform.position = dir * Time.deltaTime * speed;
+        transform.position += dir * Time.deltaTime * speed;
     }
 }
